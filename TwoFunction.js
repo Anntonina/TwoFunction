76c15130845d4ca8ffb3e52e0c6972a39b0546e7
Anntonina <30556847+Anntonina@users.noreply.github.com>
2017-08-01 22:23:31 +0300

    Учебный скрипт с двумя связанными функциями

---
+++ TwoFunction.js
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <script landuage="JavaScript">
+      function FuelCons(){
+        var Cons=prompt("Каков Ваш пробег в сотнях км?", "Пробег");
+        /*функция выводит результат, запускает вторую функцию
+        имеет свою переменную для передачи*/
+        var ans=Calcul(Cons);
+        alert("Расход горючего составил "+ ans);
+        }
+        //собственно расчет, здесь нужна переменная из перой ф.
+      function Calcul(Cons){
+        //я сразу писала var res=...
+        var res;
+        res=6*Cons;
+        //чтобы верyть значение, мы же не исп alert
+        return res;
+        //Получается в первую функцию не вставляем переменную из второй
+      }
+    </script>
+  </head>
+  <!--запускаем функцию при загрузке страницы -->
+  <body onLoad="FuelCons()">
+    <h1>Hello Plunker!</h1>
+  </body>
+
+</html>
